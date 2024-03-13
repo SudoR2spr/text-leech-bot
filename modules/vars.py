1109380b2a981f235b0dfa217ba8ce46dfb99ca4
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-03-14 01:29:55 +0530

    Text-leech-bot 🔧

---
+++ modules/vars.py
@@ -0,0 +1,6 @@
+# SudoR2spr WOODcraft
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+
+api_id = "1701393"
+api_hash = "96089a340f5892fd06aea683cbfb73c6"
+bot_token = "6872135659:AAwwm1jsMGNOFJnuICL_a1coI6CI6udAY6F4"
