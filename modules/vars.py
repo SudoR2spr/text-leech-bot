3f5056e4fa3b65e8bb7846c53fb70aa59d1342d8
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-04-25 19:33:13 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,6 +1,6 @@
 # SudoR2spr WOODcraft
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "1701393"
-api_hash = "96089a340f5892fd06aea683cbfb73c6"
-bot_token = "6872135659:AAwwm1jsMGNOFJnuICL_a1coI6CI6udAY6F4"
+api_id = "23163380"
+api_hash = "2dca155e786c7db2d295e5b4ab10783b"
+bot_token = "7055338137:AAFgB8eDvyEo4JZQbgtmGoz4P8lngDgRHsc"
