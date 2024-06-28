852b1dfc7ba675b7e3b4d78e57b592a8f339ff39
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-28 23:56:42 +0530

    Delete modules/route.py

--- modules/route.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-async def web_server():
-    app = web.Application()
-    app.router.add_get('/', hello)
-    return app
-
-async def hello(request):
-    return web.Response(text="https://github.com/AshutoshGoswami24 && https://github.com/SudoR2spr")
