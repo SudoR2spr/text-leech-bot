b1b97ba7f9bc7dce43687e33494ed0390b61825d
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-08-07 02:34:14 +0530

    utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,4 @@
-#@SudoR2spr
+# @SudoR2spr
 import time
 import math
 import os
@@ -18,7 +18,6 @@ class Timer:
 
 from datetime import datetime,timedelta
 
-#lets do calculations
 def hrb(value, digits= 2, delim= "", postfix=""):
     """Return a human-readable file size.
     """
@@ -67,7 +66,6 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
-# Powered By Ankush
 async def progress_bar(current, total, reply, start):
     if timer.can_send():
         now = time.time()
@@ -93,7 +91,6 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "◆" * completed_length + "◇" * remaining_length
             
             try:
-                await reply.edit(f'\n `╭─⌯══⟰ 𝐔𝐩𝐥𝐨𝐝𝐢𝐧𝐠 ⟰══⌯──★ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n`├𝐁𝐲 » 𝐖𝐃 𝐙𝐎𝐍𝐄\n╰─══ ✪ @Opleech_WD ✪ ══─★\n') 
+                await reply.edit(f'<b>\n ╭─⌯══⟰ 𝐔𝐩𝐥𝐨𝐝𝐢𝐧𝐠 ⟰══⌯──★ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├𝐁𝐲 » 𝐖𝐃 𝐙𝐎𝐍𝐄\n╰─══ ✪ @Opleech_WD ✪ ══─★\n</b>') 
             except FloodWait as e:
                 time.sleep(e.x)
-
