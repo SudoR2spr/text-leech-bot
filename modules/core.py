da77fb5e06c0805ab37157d9330211e9bce522bc
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-08-07 02:25:05 +0530

    core.py

--- modules/core.py
+++ modules/core.py
@@ -194,7 +194,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:12 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
-    reply = await m.reply_text(f"**Uploading ...** - `{name}`")
+    reply = await m.reply_text(f"**⥣ Uploading...** » `{name}`")
     try:
         if thumb == "no":
             thumbnail = f"{filename}.jpg"
